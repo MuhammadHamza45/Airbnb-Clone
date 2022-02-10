30b740a36972d2067db395d0ea5b89df9a74bc6a
MuhammadHamza45 <97233985+MuhammadHamza45@users.noreply.github.com>
2022-02-10 16:32:21 +0500

    login

--- signUpBackend.js
+++ signUpBackend.js
@@ -1,6 +1,7 @@
 const express=require('express');
 var mysql = require('mysql');
 const bodyParser= require('body-parser');
+const bcrypt=require('bcrypt');
 const app=express();
 app.use(bodyParser.urlencoded({
   extended:true
@@ -21,7 +22,7 @@ conn.connect(function(err) {
 app.get('/signup',(req,res)=>{    
     res.sendFile(__dirname+"/signup.html");
 });
-app.post('/signup',(req,res)=>{
+app.post('/signup',async(req,res)=>{
   var name = req.body.name;
     var email=req.body.email;
     var country = req.body.country;
@@ -29,11 +30,46 @@ app.post('/signup',(req,res)=>{
     var gender = req.body.gender;
     var dob = req.body.dob;
     var password = req.body.password;
- 
-    conn.query( `INSERT INTO signup(name,email,country,cell,gender,dob,password) VALUES ("${name}","${email}","${country}","${cell}","${gender}","${dob}","${password}")`);
+    const salt = await bcrypt.genSalt(6);
+    const hashed = await bcrypt.hash(password,salt);
+    conn.query( `INSERT INTO signup(name,email,country,cell,gender,dob,password) VALUES ("${name}","${email}","${country}","${cell}","${gender}","${dob}","${hashed}")`);
   res.send(req.body);    
   
     
+});
+
+app.get('/login',(req,res)=>{    
+  res.render("/login.html");
+});
+
+app.post('/login',async(req,res)=>{    
+  try {
+    
+    const email=req.body.email;
+    const password=req.body.password;
+    // console.log(email);
+    const sql = 'SELECT * FROM signup where email=?';
+    await conn.query(sql,[email],async(err,result)=>{
+      if(err) throw err;
+      // res.send(result);
+      if(result){
+        const inValid = await bcrypt.compare(password,result[0].password);
+        if (inValid){
+         res.send("valid ");
+        }
+        else{
+          res.send("invalid login");
+        }
+      }
+      else{
+        res.send("invalid login");
+      }
+    });
+ 
+
+  } catch (error) {
+    res.status(400).send("something roke:");
+  }
 });
 
 
